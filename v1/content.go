7e9f5d1204cdd080a5b765c32307894279ec1c1c
Brian W. Wolter <brianwolter@gmail.com>
2024-06-06 08:03:54 -0400

    Init

---
+++ v1/content.go
@@ -0,0 +1,133 @@
+package content
+
+import (
+	"database/sql/driver"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/bww/go-mime/v1"
+	textutil "github.com/bww/go-util/v1/text"
+)
+
+var ErrIncompatibleFormats = errors.New("Content formats are not compatible")
+
+var errContentTypeRequired = errors.New("Content type is required")
+
+type Content struct {
+	Type mime.Type `json:"type"`
+	Data string    `json:"data"`
+}
+
+func NewText(d string) *Content {
+	return New(mime.Text, d)
+}
+
+func NewMarkdown(d string) *Content {
+	return New(mime.Markdown, d)
+}
+
+func New(t mime.Type, d string) *Content {
+	return &Content{
+		Type: t,
+		Data: d,
+	}
+}
+
+func Join(data []Content, sep string) (Content, error) {
+	if len(data) == 0 {
+		return Content{}, nil
+	}
+	var res Content
+	for i, e := range data {
+		if i == 0 {
+			res.Type = e.Type
+		} else if e.Type != res.Type {
+			return Content{}, ErrIncompatibleFormats
+		} else {
+			res.Data += sep
+		}
+		res.Data += e.Data
+	}
+	return res, nil
+}
+
+func (d Content) Validate() error {
+	if len(d.Data) > 0 && len(d.Type) < 1 { // if we have data but no type, oh, you better believe that's an error
+		return errContentTypeRequired
+	} else {
+		return nil
+	}
+}
+
+func (d Content) IsZero() bool {
+	return len(d.Data) == 0
+}
+
+func (d Content) String() string {
+	switch d.Type {
+	case mime.Text, mime.Markdown:
+		return d.Data
+	default:
+		v, err := d.MarshalColumn()
+		if err != nil {
+			return fmt.Sprintf("<could not encode: %v>", err)
+		} else {
+			return string(v)
+		}
+	}
+}
+
+func (d Content) MarshalColumn() ([]byte, error) {
+	if d.Type == "" && len(d.Data) < 1 {
+		return []byte{}, nil
+	}
+	b := &strings.Builder{}
+	b.WriteString(d.Type.String())
+	b.WriteString(",")
+	if d.Data != "" {
+		b.WriteString(base64.StdEncoding.EncodeToString([]byte(d.Data)))
+	}
+	return []byte(b.String()), nil
+}
+
+func (d *Content) UnmarshalColumn(text []byte) error {
+	if len(text) < 1 {
+		return nil
+	}
+	x := strings.Index(string(text), ",")
+	if x < 0 {
+		return fmt.Errorf("Malformed content data: %s", textutil.Truncate(string(text), 100, " [...]"))
+	}
+	dec, err := base64.StdEncoding.DecodeString(string(text[x+1:]))
+	if err != nil {
+		return err
+	}
+	*d = Content{
+		Type: mime.Type(text[:x]),
+		Data: string(dec),
+	}
+	return nil
+}
+
+func (d Content) Value() (driver.Value, error) {
+	data, err := d.MarshalColumn()
+	if err != nil {
+		return nil, err
+	}
+	return string(data), nil
+}
+
+func (d *Content) Scan(src interface{}) error {
+	var err error
+	switch c := src.(type) {
+	case []byte:
+		err = d.UnmarshalColumn(c)
+	case string:
+		err = d.UnmarshalColumn([]byte(c))
+	default:
+		err = fmt.Errorf("Unsupported type: %T", src)
+	}
+	return err
+}
---
+++ v1/content_test.go
@@ -0,0 +1,152 @@
+package content
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContent(t *testing.T) {
+	tests := []struct {
+		Content Content
+		Expect  []byte
+		Error   error
+	}{
+		{
+			Content{
+				Type: "",
+				Data: "",
+			},
+			[]byte(""),
+			nil,
+		},
+		{
+			Content{
+				Type: "hello/there",
+				Data: "",
+			},
+			[]byte("hello/there,"),
+			nil,
+		},
+		{
+			Content{
+				Type: "",
+				Data: "Cooltown okaysville",
+			},
+			[]byte(",Q29vbHRvd24gb2theXN2aWxsZQ=="),
+			nil,
+		},
+		{
+			Content{
+				Type: "hello/there;charset=utf-8",
+				Data: "",
+			},
+			[]byte("hello/there;charset=utf-8,"),
+			nil,
+		},
+		{
+			Content{
+				Type: "hello/there",
+				Data: "Cooltown okaysville",
+			},
+			[]byte("hello/there,Q29vbHRvd24gb2theXN2aWxsZQ=="),
+			nil,
+		},
+		{
+			Content{
+				Type: "hello/there;charset=utf-8",
+				Data: "Cooltown okaysville",
+			},
+			[]byte("hello/there;charset=utf-8,Q29vbHRvd24gb2theXN2aWxsZQ=="),
+			nil,
+		},
+		{
+			Content{
+				Type: " hello / there; charset=utf-8 ",
+				Data: "Cooltown okaysville",
+			},
+			[]byte(" hello / there; charset=utf-8 ,Q29vbHRvd24gb2theXN2aWxsZQ=="),
+			nil,
+		},
+	}
+	for _, e := range tests {
+		enc, err := e.Content.MarshalColumn()
+		if e.Error != nil {
+			fmt.Println("***", err)
+			assert.Equal(t, e.Error, err)
+		} else if assert.Nil(t, err, fmt.Sprint(err)) {
+			fmt.Println("-->", string(enc))
+			assert.Equal(t, e.Expect, enc)
+		}
+		if err != nil {
+			dec := &Content{}
+			err = dec.UnmarshalColumn(enc)
+			if assert.Nil(t, err, fmt.Sprint(err)) {
+				assert.Equal(t, e.Content, dec)
+			}
+		}
+	}
+}
+
+func TestJoinContent(t *testing.T) {
+	tests := []struct {
+		Data   []Content
+		Sep    string
+		Expect Content
+		Error  func(error) error
+	}{
+		{
+			[]Content{
+				{Type: "", Data: ""},
+			},
+			"",
+			Content{Type: "", Data: ""},
+			nil,
+		},
+		{
+			[]Content{
+				{Type: "a", Data: "First"},
+				{Type: "a", Data: "Second"},
+			},
+			"/",
+			Content{Type: "a", Data: "First/Second"},
+			nil,
+		},
+		{
+			[]Content{
+				{Type: "a", Data: "First"},
+				{Type: "a", Data: "Second"},
+				{Type: "a", Data: "Third"},
+			},
+			"/",
+			Content{Type: "a", Data: "First/Second/Third"},
+			nil,
+		},
+		{
+			[]Content{
+				{Type: "a", Data: "First"},
+				{Type: "b", Data: "Second"},
+				{Type: "a", Data: "Third"},
+			},
+			"/",
+			Content{},
+			func(err error) error {
+				if errors.Is(err, ErrIncompatibleFormats) {
+					return nil
+				} else {
+					return err
+				}
+			},
+		},
+	}
+	for _, e := range tests {
+		res, err := Join(e.Data, e.Sep)
+		if e.Error != nil {
+			assert.Nil(t, e.Error(err), fmt.Sprint(err))
+		} else {
+			assert.Equal(t, e.Expect, res)
+		}
+	}
+}
